35659a1ff0fba8c1c3d3de0cf57a3143c873d8e2
juuuuurien <97487768+juuuuurien@users.noreply.github.com>
2022-07-24 12:13:56 -0700

    Moved middleware.js to src

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,5 @@
 export { default } from "next-auth/middleware";
-
 export const config = {
   matcher: ["/dashboard"],
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
 };
