0f4e4d2950aba86c1407787775dcfa36eb1cf8bb
Julien Lopez <Julien24Lopez@gmail.com>
2022-08-07 15:07:20 -0700

    Started refactoring landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,17 +15,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className=" flex flex-col h-screen w-screen py-20  bg-green-900">
+      <div className="flex flex-col h-full w-full ">
         {/* navbar */}
-        <div className="fixed flex flex-row justify-between items-center self-center lg:gap-20 p-2 lg:p-[12px] w-[90%] xl:w-[66%] rounded-[10000px] bg-gray-200">
+        <div className="flex flex-row px-5  justify-between items-center w-full bg-gray-100 p-2">
           <div className="flex flex-row items-center gap-10 ">
-            <button className=" mx-5 lg:w-[200px] w-[100px]">
+            <button className="mx-5 lg:w-[200px] w-[100px]">
               <Link href="/dashboard">
                 <Image
                   src={Brand}
-                  height={80}
-                  width={200}
+                  height={36}
+                  width={164}
                   layout="responsive"
+                  alt="Logo"
                 />
               </Link>
             </button>
@@ -63,7 +64,7 @@ export default function Home() {
           </div>
         </div>
         {/* Hero Page */}
-        <div className="flex flex-col h-full w-full text-center justify-center md:justify-start md:text-left pt-[10vh] sm:pt-[14vh] md:pt-[18vh] px-[15vw] ">
+        {/* <div className="flex flex-col h-full w-full text-center justify-center md:justify-start md:text-left pt-[10vh] sm:pt-[14vh] md:pt-[18vh] px-[15vw] ">
           <section className="flex flex-col gap-5 lg:max-w-[50%]">
             <p className="xl:jumbo-text font-bold text-5xl md:text-6xl lg:text-8xl text-yellow-400">
               Everything you are. All in one simple link.
@@ -73,7 +74,33 @@ export default function Home() {
               online. All from the one link in your bio.
             </p>
           </section>
-        </div>
+        </div> */}
+        <section className="relative h-[560px] w-full bg-slate-400 overflow-hidden">
+          <div className="absolute flex justify-start items-center h-full w-full bg-[#dde8f3cc]">
+            <div className="flex flex-col gap-5 lg:max-w-[50%] mx-[10%] ">
+              <p className="xl:jumbo-text font-bold text-5xl md:text-6xl lg:text-8xl text-gray-800">
+                All of who you are in{" "}
+                <span className="text-[#3395FF]">one simple link.</span>
+              </p>
+              <p className="text-slate-800 text-left font-semibold text-xl md:max-w-[80%]">
+                Join linkspring to start networking with thousands of people,
+                all with a simple link in your bio.
+              </p>
+            </div>
+          </div>
+          <video
+            autoPlay
+            loop
+            style={{ postison: "absolute", width: "100%", height: "auto" }}
+          >
+            <source
+              src={
+                "https://res.cloudinary.com/linkspring-me/video/upload/v1659810286/hero_background_video_jjqlvu.mp4"
+              }
+              type="video/mp4"
+            />
+          </video>
+        </section>
       </div>
     </div>
   );
