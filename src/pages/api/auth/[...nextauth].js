d8ce77d600a9c5da8f34bf899f53020ceac871af
Julien Lopez <97487768+juuuuurien@users.noreply.github.com>
2022-08-16 23:34:27 -0700

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -12,13 +12,6 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
